ffc52bb8d8ea45dd7ab0b1b51c853324f8c75d96
Nadia Salmen <nadiasalmen@gmail.com>
2023-11-24 18:42:32 -0300

    Add notification channels to followers when creating new recipe

---
+++ app/models/notification.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Notification < ApplicationRecord
+  belongs_to :user
+  after_create :stream_notification
+
+  private
+
+  def stream_notification
+    user.followers.each do |follower|
+      message_data = {
+        user_id: user_id,
+        message: message,
+        follower_id: follower.id
+      }
+      ActionCable.server.broadcast "notifications_#{follower_id}", message_data
+    end
+  end
+end
